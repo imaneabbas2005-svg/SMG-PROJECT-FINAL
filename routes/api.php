3c889e013d1866abbc8c3e5a70256126b0309814
imaneabbas2005-svg <imaneabbas2005@gmail.com>
2026-05-15 19:00:22 +0300

    update project

--- routes/api.php
+++ routes/api.php
@@ -8,9 +8,11 @@ Route::get('/resources', function (Request $request) {
 
     $query = Resource::where('is_approved', true);
 
-    if ($request->category) {
-        $query->where('category_id', $request->category);
-    }
+   if ($request->category) {
+    $query->whereHas('category', function ($q) use ($request) {
+        $q->where('name', $request->category);
+    });
+}
 
     return $query->paginate(5);
 });
